ff30216de6147ed2557bff63d4aa5a5df74e1d44
SvenEST <sven.veski@gmail.com>
2017-03-03 09:17:27 +0200

    Aurelia Routing: õppevideodest
    Routingu lisamine app.html ja app.js failidesse. Lisaks ka esimese
    alamlehe home lihtne ülesehitus

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -1,21 +1,31 @@
 define('app',['exports'], function (exports) {
-  'use strict';
+    'use strict';
 
-  Object.defineProperty(exports, "__esModule", {
-    value: true
-  });
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
 
-  function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
     }
-  }
 
-  var App = exports.App = function App() {
-    _classCallCheck(this, App);
+    var App = exports.App = function () {
+        function App() {
+            _classCallCheck(this, App);
 
-    this.message = 'Hello World!';
-  };
+            this.message = 'Welcome to Estlingo!';
+        }
+
+        App.prototype.configureRouter = function configureRouter(config, router) {
+            this.router = router;
+            config.title = 'Estlingo';
+            config.map([{ route: ['', 'home'], name: 'home', moduleId: 'home/index' }]);
+        };
+
+        return App;
+    }();
 });
 define('environment',["exports"], function (exports) {
   "use strict";
@@ -75,5 +85,45 @@ define('resources/index',["exports"], function (exports) {
   exports.configure = configure;
   function configure(config) {}
 });
-define('text!app.html', ['module'], function(module) { module.exports = "<template><h1>${message}</h1></template>"; });
+define('index',['exports'], function (exports) {
+    'use strict';
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
+
+    var App = exports.App = function App() {
+        _classCallCheck(this, App);
+
+        this.message = 'Welcome to Estlingo!';
+    };
+});
+define('home/index',["exports"], function (exports) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
+
+    var Home = exports.Home = function Home() {
+        _classCallCheck(this, Home);
+
+        this.message = "Check, check!";
+    };
+});
+define('text!app.html', ['module'], function(module) { module.exports = "<template><router-view></router-view><h1>${message}</h1></template>"; });
+define('text!index.html', ['module'], function(module) { module.exports = "<template><router-view></router-view><h1>${message}</h1></template>"; });
+define('text!home/index.html', ['module'], function(module) { module.exports = "<template>Tere! ${message}</template>"; });
 //# sourceMappingURL=app-bundle.js.map
--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
-  }
+    constructor() {
+        this.message = 'Welcome to Estlingo!';
+    }
+
+    configureRouter(config, router) {
+        this.router = router;
+        config.title = 'Estlingo';
+        config.map([
+            { route: ['', 'home'],       name: 'home',       moduleId: 'home/index' },
+            ]);
+    }
 }
