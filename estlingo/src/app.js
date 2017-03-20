7494f9bd234a763526d5988ad7f41aa32e4a11fc
SvenEST <sven.veski@gmail.com>
2017-03-20 20:34:59 +0200

    ...

--- estlingo/src/app.js
+++ estlingo/src/app.js
@@ -4,11 +4,11 @@ export class App {
         this.router = router;
         config.title = 'Estlingo';
         config.map([
-            { route: ['', 'register'],       name: 'register',       moduleId: 'register/register' },
+            { route: ['', 'register'],   name: 'register',       moduleId: 'register/register' },
             { route: 'games',            name: 'games',      moduleId: 'games/games',   nav: true },
             { route: 'about',            name: 'about',      moduleId: 'about/about',   nav: true },
-            { route: 'home',         name: 'home',   moduleId: 'home/home',   nav: true },
-            { route: 'select', 			 name: 'select',     moduleId: 'games/select',   nav: true },
+            { route: 'home',             name: 'home',   moduleId: 'home/home',   nav: true },
+            { route: 'select',             name: 'select',   moduleId: 'select/select',   nav: true },
             ]);
     }
 }
