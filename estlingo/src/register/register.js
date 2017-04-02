82853a4e033537774d0c42b900ead171d960d58b
SvenEST <sven.veski@gmail.com>
2017-04-02 12:49:51 +0300

    Parandused ja regamise arendus
    osati poolik, serveriga ei saa vahel ühendust

--- estlingo/src/register/register.js
+++ estlingo/src/register/register.js
@@ -9,17 +9,33 @@ export class Register{
     }
     
     addUser() {
+        console.log("addUser() started");
         let client = new HttpClient();
-
-        client.fetch('http://localhost:8080/users/add', {
+        client.fetch("http://localhost:8080/users/add", {
             'method': "POST",
             'body': json(this.userData)
         })
             .then(response => response.json())
             .then(data => {
-                console.log("Server saatis " + data.username + data.password + data.eMail);
+                console.log("Server saatis " + data.username + data.password + data.e_mail);
         });
+        
+        console.log("addUser() finished");
+    }
 
-        console.log("Method executed!")
+    getUser(username, password){
+        console.log("getUser() started");
+        let client = new HttpClient();
+        client.fetch("http://localhost:8080/users/" + username)
+            .then (response => response.json())
+            .then (data => {
+                if (JSON.stringify(data.password) == password){
+                    window.location.href = "http://localhost:9000/#/home";
+                }else{
+                    alert("No such user or password exists!")
+                }
+            })
+        
+        console.log("getUser() finished");
     }
 }
