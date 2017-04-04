1e204e1a397e4e443511b8d65bdda82ce708b30e
TauriTurkson <taurionmunimi@gmail.com>
2017-04-05 00:06:25 +0300

    Games fix

--- estlingo/src/app.js
+++ estlingo/src/app.js
@@ -5,10 +5,10 @@ export class App {
         config.title = 'Estlingo';
         config.map([
             { route: ['', 'register'],   name: 'register',       moduleId: 'register/register' },
-            { route: 'games',            name: 'games',      moduleId: 'games/games',   nav: true },
-            { route: 'about',            name: 'about',      moduleId: 'about/about',   nav: true },
-            { route: 'home',             name: 'home',   moduleId: 'home/home',   nav: true },
-            { route: 'select',             name: 'select',   moduleId: 'select/select',   nav: true },
+            { route: 'games',            name: 'games',          moduleId: 'games/games',   nav: true },
+            { route: 'about',            name: 'about',          moduleId: 'about/about',   nav: true },
+            { route: 'home',             name: 'home',           moduleId: 'home/home',     nav: true },
+            { route: 'select',           name: 'select',         moduleId: 'select/select', nav: true },
             ]);
     }
 }
