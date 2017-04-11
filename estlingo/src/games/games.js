13da67c307916953ffb484cffd7cc856e47e16bb
SvenEST <sven.veski@gmail.com>
2017-04-11 19:41:13 +0300

    SelectEquivalent2

--- estlingo/src/games/games.js
+++ estlingo/src/games/games.js
@@ -2,6 +2,7 @@ export class games{
     
     constructor(){
         this.h1 = "Select equivalent"
-		this.h2 = "Type equivalent"
+        this.h2 = "Select equivalent 2"
+		this.h3 = "Type equivalent"
     }
 }
