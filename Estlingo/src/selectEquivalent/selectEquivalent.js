e2bd592f3f8b0039f8c23ee1c67e532e5e7de8a2
SvenEST <sven.veski@gmail.com>
2017-05-15 18:16:25 +0300

    SelectEquivalent mängude peitmine skoori alusel

--- Estlingo/src/selectEquivalent/selectEquivalent.js
+++ Estlingo/src/selectEquivalent/selectEquivalent.js
@@ -1,4 +1,15 @@
+import {AureliaCookie} from 'aurelia-cookie';
+
 export class type{
+    
+    score;
+    apple;
+    chair;
+    bicycle;
+    potato;
+    strawberry;
+    clock;
+    fork;
     
     constructor(){
     	this.h1 = "Apple"
@@ -8,5 +19,39 @@ export class type{
     	this.h5 = "Strawberry"
     	this.h6 = "Clock"
         this.h7 = "Fork"
+        
+        this.score = 0;
+        
+        if(!(AureliaCookie.get('score') == null)){
+            this.score = AureliaCookie.get('score');
+        }
+        
+        this.apple = true;
+        
+        console.log("score: " + this.score);
+        
+        if(this.score > 10){
+            this.chair = true;
+        }
+        
+        if(this.score > 20){
+            this.bicycle = true;
+        }
+        
+        if(this.score > 30){
+            this.potato = true;
+        }
+        
+        if(this.score > 40){
+            this.strawberry = true;
+        }
+        
+        if(this.score > 50){
+            this.clock = true;
+        }
+        
+        if(this.score > 60){
+            this.fork = true;
+        }
     }
 }
