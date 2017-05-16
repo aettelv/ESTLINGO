291f570d033971ec9af3dbdddd7da3138f045ef5
TauriTurkson <taurionmunimi@gmail.com>
2017-05-16 21:05:21 +0300

    about & contact pildid

--- Estlingo/src/contact/contact.js
+++ Estlingo/src/contact/contact.js
@@ -1,8 +1,25 @@
+import {AureliaCookie} from 'aurelia-cookie';
+import {HttpClient, json} from "aurelia-fetch-client";
+
 export class about{
     
     constructor(){
         this.message = "Contact us:"
         this.email = "Email: estlingo@gmail.com"
-        this.phone = "Phone: +372 123456789"
+        this.phone = "Phone: +372 1234 5678"
+
+        let client1 = new HttpClient();
+        client1.fetch("http://localhost:8080/pictures/email")
+            .then (response => response.json())
+            .then (data => {
+                this.location1 = data.path;
+            })
+
+        let client2 = new HttpClient();
+        client2.fetch("http://localhost:8080/pictures/phone")
+            .then (response => response.json())
+            .then (data => {
+                this.location2 = data.path;
+            })
     }
 }
--- estlingo/src/about/about.js
+++ estlingo/src/about/about.js
@@ -1,3 +1,6 @@
+import {AureliaCookie} from 'aurelia-cookie';
+import {HttpClient, json} from "aurelia-fetch-client";
+
 export class about{
     
     constructor(){
@@ -6,5 +9,35 @@ export class about{
 		this.fast = "Are you having a coffee break? Good, then you have more than enough time to play one of our games and educate yourself."
 		this.family = "Language learning by games is so easy, that even kids can learn Estonian. Why not play language games with the whole family?"
 		this.game = "Do you love games and puzzles? Then this is the right place for you!"
+
+
+	    let client = new HttpClient();
+        client.fetch("http://localhost:8080/pictures/easy")
+            .then (response => response.json())
+            .then (data => {
+                this.location1 = data.path;
+            })
+
+        let client2 = new HttpClient();
+        client2.fetch("http://localhost:8080/pictures/fast")
+            .then (response => response.json())
+            .then (data => {
+                this.location2 = data.path;
+            })
+
+        let client3 = new HttpClient();
+        client3.fetch("http://localhost:8080/pictures/family")
+            .then (response => response.json())
+            .then (data => {
+                this.location3 = data.path;
+            })
+
+        let client4 = new HttpClient();
+        client4.fetch("http://localhost:8080/pictures/game")
+            .then (response => response.json())
+            .then (data => {
+                this.location4 = data.path;
+            })
     }
+
 }
